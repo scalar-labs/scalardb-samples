934eec073d62baa3e501b999149ea0b3af8152a2
Mitsunori Komatsu <komamitsu@gmail.com>
2023-07-10 11:13:34 +0900

    Use new high-level 2PC API in the sample application for microservice with Spring Data JDBC for ScalarDB (#42)

--- spring-data-microservice-transaction-sample/customer-service/src/main/java/sample/customer/domain/repository/CustomerRepository.java
+++ spring-data-microservice-transaction-sample/customer-service/src/main/java/sample/customer/domain/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package sample.customer.domain.repository;
 
 import com.scalar.db.sql.springdata.twopc.ScalarDbTwoPcRepository;
-import java.util.function.Supplier;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import sample.customer.domain.model.Customer;
@@ -14,18 +13,5 @@ public interface CustomerRepository extends ScalarDbTwoPcRepository<Customer, In
     if (!findById(customer.customerId).isPresent()) {
       insert(customer);
     }
-  }
-
-  default <T> T execOneshotOperation(Supplier<T> task) {
-    begin();
-    T result = task.get();
-    prepare();
-    validate();
-    commit();
-    return result;
-  }
-
-  default <T> T execBatchOperations(Supplier<T> task) {
-    return task.get();
   }
 }
--- spring-data-microservice-transaction-sample/order-service/src/main/java/sample/order/domain/repository/OrderRepository.java
+++ spring-data-microservice-transaction-sample/order-service/src/main/java/sample/order/domain/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package sample.order.domain.repository;
 
 import com.scalar.db.sql.springdata.twopc.ScalarDbTwoPcRepository;
 import java.util.List;
-import java.util.function.Supplier;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import sample.order.domain.model.Order;
@@ -12,13 +11,4 @@ import sample.order.domain.model.Order;
 public interface OrderRepository extends ScalarDbTwoPcRepository<Order, String> {
 
   List<Order> findAllByCustomerIdOrderByTimestampDesc(int customerId);
-
-  default <T> T execOneshotOperation(Supplier<T> task) {
-    begin();
-    T result = task.get();
-    prepare();
-    validate();
-    commit();
-    return result;
-  }
 }
