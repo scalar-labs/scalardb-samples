8aeb6bda42d19c1463840ec7c13267a6728523c5
Akihiro Okuno <akihiro.okuno@scalar-labs.com>
2024-05-09 16:21:12 +0900

    DLT-15030 Prepare sample application of ScalarDB Analytics with Spark (#58)
    * Add sample application of ScalarDB Analytics with Spark
    
    * Apply suggestions from code review
    
    Co-authored-by: Josh Wong <joshua.wong@scalar-labs.com>
    
    * Use eclipse temurin as base JDK image
    
    * Do not expose port to host
    
    * Add healthcheck if avaialable
    
    * Change service name
    
    ---------
    
    Co-authored-by: Josh Wong <joshua.wong@scalar-labs.com>

---
+++ scalardb-analytics-spark-sample/README.md
@@ -0,0 +1,5 @@
+# Sample Application of ScalarDB Analytics with Spark
+
+This sample application demonstrates how you can run analytical queries on data that is managed by [ScalarDB](https://www.scalar-labs.com/scalardb/) by using [ScalarDB Analytics with Spark](https://scalardb.scalar-labs.com/docs/latest/scalardb-analytics-spark/getting-started/).
+
+For more information, please refer to the [tutorial](https://scalardb.scalar-labs.com/docs/latest/scalardb-samples/scalardb-analytics-spark-sample/).
---
+++ scalardb-analytics-spark-sample/sample-data-loader/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.diffplug.spotless") version "6.24.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.scalar-labs:scalardb:3.12.1")
+    implementation("com.scalar-labs:scalardb-schema-loader:3.12.1")
+    implementation("org.apache.commons:commons-csv:1.10.0")
+
+    implementation("io.netty:netty-transport-native-epoll:4.1.99.Final:linux-x86_64")
+    implementation("io.netty:netty-transport-native-epoll:4.1.99.Final:linux-aarch_64")
+    implementation("io.netty:netty-transport-native-kqueue:4.1.99.Final:osx-x86_64")
+    implementation("io.netty:netty-transport-native-kqueue:4.1.99.Final:osx-aarch_64")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("sample.data.Main")
+}
+
+spotless {
+    java {
+        target("src/*/java/**/*.java")
+        importOrder()
+        removeUnusedImports()
+        googleJavaFormat("1.17.0")
+    }
+}
