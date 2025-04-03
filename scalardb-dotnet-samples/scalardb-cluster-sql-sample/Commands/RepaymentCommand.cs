032c1196a1e18d123da71f9d4d8c027d4040a077
Dima <72738687+Dima-X@users.noreply.github.com>
2025-04-03 16:06:45 +0900

    Add sample application for ScalarDB Cluster .NET Client SDK (SQL)

---
+++ scalardb-dotnet-samples/scalardb-cluster-sql-sample/Commands/RepaymentCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.CommandLine;
+
+namespace ScalarDbClusterSqlSample.Commands;
+
+public static class RepaymentCommand
+{
+    private const string Name = "Repayment";
+    private const string Description = "Repayment";
+
+    private const string ArgName1 = "customer_id";
+    private const string ArgDescription1 = "customer ID";
+    private const string ArgName2 = "amount";
+    private const string ArgDescription2 = "amount of the money for repayment";
+
+    public static Command Create()
+    {
+        var customerIdArg = new Argument<int>(ArgName1, ArgDescription1);
+        var amountArg = new Argument<int>(ArgName2, ArgDescription2);
+        var repaymentCommand = new Command(Name, Description)
+        {
+            customerIdArg,
+            amountArg
+        };
+
+        repaymentCommand.SetHandler(async (customerId, amount) =>
+        {
+            using var sample = new Sample();
+            await sample.Repayment(customerId, amount);
+        }, customerIdArg, amountArg);
+
+        return repaymentCommand;
+    }
+}
